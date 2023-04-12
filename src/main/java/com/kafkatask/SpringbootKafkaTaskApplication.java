8c507d746386f1f95b6bfff1d2dc45535b079a22
pradyumna1979 <kuni.khadanga@gmail.com>
2023-04-12 18:18:07 +0530

    Kafka producer

---
+++ src/main/java/com/kafkatask/SpringbootKafkaTaskApplication.java
@@ -0,0 +1,13 @@
+package com.kafkatask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafkatask/SpringbootKafkaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkatask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
